50cc5cb95a7e146c00a83b5a5af8bc13f63d3d08
PPSasso <pp_sasso@hotmail.com>
2021-04-11 22:37:34 -0300

    Sidebar v1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <my-header></my-header>
+  <div>
+    <my-header></my-header>
+    <my-sidebar></my-sidebar>
+  </div>
+  
 </template>
 
 <script>
 import MyHeader from './components/MyHeader.vue'
+import MySidebar from './components/MySidebar.vue'
 
 export default {
   name: 'App',
   components: {
-    MyHeader
+    MyHeader,
+    MySidebar
   }
 }
 </script>
---
+++ src/components/MySidebar.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="sidebar-page">
+        <section class="sidebar-layout">
+            <b-sidebar 
+                fullheight="true"
+                position="fixed"
+                :mobile="mobile"
+                :expand-on-hover="expandOnHover"
+                :reduce="reduce"
+                open
+            >
+                <div class="p-1">
+                    <b-menu class="is-custom-mobile">
+                        <b-menu-list>
+                            <b-menu-item icon-pack="fas" icon="fas fa-home" label="Dashboard"></b-menu-item>
+                            <b-menu-item icon-pack="fas" icon="fas fa-users" label="Grupos"></b-menu-item>
+                            <b-menu-item icon-pack="fas" icon="fas fa-user" label="Usuarios"></b-menu-item>
+                            <b-menu-item icon-pack="fas" icon="fas fa-cog" label="Sistemas"></b-menu-item>
+                            <b-menu-item icon-pack="fas" icon="fas fa-network-wired" label="Categoria"></b-menu-item>
+                        </b-menu-list>
+                    </b-menu>
+                </div>
+            </b-sidebar>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            expandOnHover: false,
+            mobile: "reduce",
+            reduce: false
+        };
+    },
+    name: "MySidebar"
+};
+</script>
+
+<style lang="scss">
+.p-1 {
+  padding: 1em;
+}
+.sidebar-page {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    min-height: 100%;
+    //min-height: 100vh;
+    .sidebar-layout {
+        display: flex;
+        flex-direction: row;
+        min-height: 0%;
+        // min-height: 100vh;
+    }
+}
+@media screen and (max-width: 1023px) {
+    .b-sidebar {
+        .sidebar-content {
+            &.is-mini-mobile {
+                &:not(.is-mini-expand),
+                &.is-mini-expand:not(:hover) {
+                    .menu-list {
+                        li {
+                            a {
+                                span:nth-child(2) {
+                                    display: none;
+                                }
+                            }
+                            ul {
+                                padding-left: 0;
+                                li {
+                                    a {
+                                        display: inline-block;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .menu-label:not(:last-child) {
+                        margin-bottom: 0;
+                    }
+                }
+            }
+        }
+    }
+}
+@media screen and (min-width: 1024px) {
+    .b-sidebar {
+        .sidebar-content {
+            &.is-mini {
+                &:not(.is-mini-expand),
+                &.is-mini-expand:not(:hover) {
+                    .menu-list {
+                        li {
+                            a {
+                                span:nth-child(2) {
+                                    display: none;
+                                }
+                            }
+                            ul {
+                                padding-left: 0;
+                                li {
+                                    a {
+                                        display: inline-block;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .menu-label:not(:last-child) {
+                        margin-bottom: 0;
+                    }
+                }
+            }
+        }
+    }
+}
+.is-mini-expand {
+    .menu-list a {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+}
+</style>
